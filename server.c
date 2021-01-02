689e0eb2fbdfea28f85d77ed8cab1c4b60a14719
Sandali Hasara Perera <56555623+SandaliPerera@users.noreply.github.com>
2021-01-02 23:18:24 +0530

    Add files via upload

---
+++ server.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <fontl.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char *argv[])
+{
+		struct socketaddr_in server_addr, client_addr;
+		
+		
+		return 0;
+}	
